eefc1bb818e8501f512a8286616f1323b4371d87
Jake <github@jakewendt.com>
2023-12-19 14:34:12 -0700

    Refining scripts

--- scripts/larman_aggregate_samples.bash
+++ scripts/larman_aggregate_samples.bash
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+
+#	PSEUDO CODE THAT I HAVE YET TO EVER RUN
+
+
+#	60 Load the resulting tab-delimited file into Python or R as a dataframe for further analysis 
+#	(e.g., the Python pandas library (https://pandas.pydata.org/) or 
+#	the R tidyverse (https://www.tidyverse.org/). 
+
+#	In Python, the command would be:
+
+#	import pandas as pd
+#	df = pd.read_csv('mlxp.tsv', sep='\t', header=0)
+
+
--- scripts/larman_process_sample.bash
+++ scripts/larman_process_sample.bash
@@ -42,16 +42,3 @@ phip merge-columns -i workdir/mlxp -o mlxp.tsv -p 1
 
 
 
-
-
-
-#	60 Load the resulting tab-delimited file into Python or R as a dataframe for further analysis 
-#	(e.g., the Python pandas library (https://pandas.pydata.org/) or 
-#	the R tidyverse (https://www.tidyverse.org/). 
-
-#	In Python, the command would be:
-
-#	import pandas as pd
-#	df = pd.read_csv('mlxp.tsv', sep='\t', header=0)
-
-
