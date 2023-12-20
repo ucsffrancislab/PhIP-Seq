d434be1d1f185ab628921c6de3d7d90c62e6c23a
Jake <github@jakewendt.com>
2023-12-20 10:59:46 -0700

    Archived

---
+++ scripts/archive/split_data_frame.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+
+df = pd.read_csv(sys.argv[1],index_col="id")
+
+for col in df.columns:
+	tmp=df[col]
+	tmp.to_csv(sys.argv[1]+"."+col+".csv")
+
