eaebb4b976f268cc7e355710a2907c9326a36242
Jake <github@jakewendt.com>
2023-12-19 20:58:58 -0700

    Cleanup and fix column name

--- scripts/merge_all_combined_counts_files.py
+++ scripts/merge_all_combined_counts_files.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 # B. Calculate Z-scores
 # Note: To perform the Z-score analysis, count.combined files are merged into a table, and columns corresponding with no-serum controls are summed in a column     called “input”.
 
@@ -30,8 +29,6 @@ parser.add_argument('files', nargs='*', help='files help')
 parser.add_argument('-V','--version', action='version', version='%(prog)s 1.1')
 parser.add_argument('-o', '--output', nargs=1, type=str, default='merged.csv.gz', help='output csv filename to %(prog)s (default: %(default)s)')
 
-#parser.add_argument('-s', '--sep', nargs=1, type=str, default='\t', help='the separator to %(prog)s (default: %(default)s)')
-
 #	store_true means "int=False unless --int passed, then int=True" (store_false is the inverse)
 parser.add_argument('--int', action='store_true', help='convert values to ints to %(prog)s (default: %(default)s)')
 
@@ -52,14 +49,6 @@ else:
 	output=args.output
 print( "Using output name: ", output )
 
-#if isinstance(args.sep,list):
-#	sep=args.sep[0]
-#else:
-#	sep=args.sep
-#print( "Using separator :", sep, ":" )
-
-
-
 
 data_frames = []
 
@@ -68,31 +57,16 @@ for filename in args.files:
 	if os.path.isfile(filename) and os.path.getsize(filename) > 0:
 		basename=os.path.basename(filename)
 
-		#sample=basename.split(".")[0]	#	everything before the first "."
 		sample=basename.split(".")[0]	#	everything before the first "."
 
 		print("Reading "+filename+": Sample "+sample)
-
-		#	sep="\t",
-		#d = pd.read_csv(filename,
-		#	skipinitialspace=True,
-		#	sep="\s+",
-		#	header=None,
-		#	usecols=[0,1],
-		#	names=[sample,'sequence'],
-		#	index_col=['sequence'])
-		#	#usecols=[0,1,2],
-		#	#names=['chromosome','position',sample],
-		#	#index_col=['chromosome','position'])
-
-		#	this is the only way to read a file separated by spaces but containing spaces.
 
 		d = pd.read_csv(filename,
 			skipinitialspace=True,
 			sep=",",
 			index_col=["id"])
 
-		d.rename(columns={"SAMPLE_ID": basename}, inplace=True)
+		d.rename(columns={"SAMPLE_ID": sample}, inplace=True)
 
 		print(d.head())
 
@@ -112,6 +86,9 @@ if len(data_frames) > 0:
 	if args.int:
 		print("Converting all counts back to integers")
 		df = pd.DataFrame(df, dtype=int)
+
+	#	don't think this is necessary
+	#df.sort_index(inplace=True)
 
 	print("Writing CSV")
 	df.to_csv(output,index_label=['id'])
