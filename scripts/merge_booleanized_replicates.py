2f55463128fdddbf155d98112a874f6fdf43a43c
Jake <jakewendt@gmail.com>
2024-05-24 20:02:22 -0700

    Testing pipeline on actual data and making mods.

--- scripts/merge_booleanized_replicates.py
+++ scripts/merge_booleanized_replicates.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(prog=os.path.basename(__file__))
 
 parser.add_argument('files', nargs='*', help='files help')
 parser.add_argument('-V','--version', action='version', version='%(prog)s 1.1')
-parser.add_argument('-o', '--output', nargs=1, type=str, default='merged.csv.gz', help='output csv filename to %(prog)s (default: %(default)s)')
+parser.add_argument('-o', '--output', nargs=1, type=str, default=['merged.csv.gz'], help='output csv filename to %(prog)s (default: %(default)s)')
 
 #	store_true means "int=False unless --int passed, then int=True" (store_false is the inverse)
 parser.add_argument('--int', action='store_true', help='convert values to ints to %(prog)s (default: %(default)s)')
@@ -23,15 +23,7 @@ parser.add_argument('--seqint', action='store_true', help='items are ints so sor
 args = parser.parse_args()
 
 
-
-#	Note that nargs=1 produces a list of one item. This is different from the default, in which the item is produced by itself.
-#	THAT IS JUST STUPID! And now I have to check it manually. Am I the only one?
-
-
-if isinstance(args.output,list):
-	output=args.output[0]
-else:
-	output=args.output
+output=args.output[0]
 print( "Using output name: ", output )
 
 
