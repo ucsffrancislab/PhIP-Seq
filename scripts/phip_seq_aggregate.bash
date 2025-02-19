b25f385f9ed4d12bdfb2965f7833d426842452ac
Jake <jakewendt@gmail.com>
2025-02-18 19:33:23 -0800

    Creating a Counts matrix annotated the same as the Zscores file

--- scripts/phip_seq_aggregate.bash
+++ scripts/phip_seq_aggregate.bash
@@ -296,7 +296,7 @@ if [ -f ${dir}/All.count.Zscores.minimums.csv ] ; then
 	cat ${dir}/Zscores.minimums.csv | datamash transpose -t, > ${dir}/Zscores.minimums.t.csv
 	head -5 ${dir}/Zscores.minimums.t.csv | cut -c1-100
 
-	#	hc out.plate1/Zscores.select.minimums.t.csv 
+	#	hc out.plate1/Zscores.select.minimums.t.csv
 	#	y,subject,14078-01,14118-01,14127-01,14142-01,14206-01,14223-01,14235-01,14337-01,14358-02,14415-02,
 	#	z,type,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,gl
 	#	id,group,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,c
@@ -312,8 +312,159 @@ fi
 
 
 
+
+
+
+#if [ -f ${dir}/Counts.csv ] ; then
+	Q=40
+	merge_all_combined_counts_files.py --de_nan --int -o ${dir}/tmp1.csv \
+	  ${dir}/counts/*.q${Q}.count.csv.gz ${dir}/counts/input/*.q${Q}.count.csv.gz
+
+	awk -F, '{print NF}' ${dir}/tmp1.csv | uniq
+	#576
+
+	wc -l ${dir}/tmp1.csv
+	#113826 tmp1.csv
+
+	head -1 ${dir}/tmp1.csv > ${dir}/tmp2.csv
+	tail -n +2 ${dir}/tmp1.csv | sort -t, -k1,1 >> ${dir}/tmp2.csv
+
+	cat ${dir}/tmp2.csv | datamash transpose -t, > ${dir}/tmp3.csv
+	head -1 ${dir}/tmp3.csv > ${dir}/tmp4.csv
+	tail -n +2 ${dir}/tmp3.csv | sort -t, -k1,1 >> ${dir}/tmp4.csv
+
+
+	awk 'BEGIN{FS=OFS=","}{print $2,$1,$3,$4,$5,$6,$7,$8,$9}' ${manifest} > ${dir}/tmpman1.csv
+	head -1 ${dir}/tmpman1.csv > ${dir}/tmpman2.csv
+	tail -n +2 ${dir}/tmpman1.csv | sort -t, -k1,1 >> ${dir}/tmpman2.csv
+
+	join --header -t, ${dir}/tmpman2.csv ${dir}/tmp4.csv > ${dir}/tmp5.csv
+
+
+
+	cat ${dir}/tmp5.csv | datamash transpose -t, > ${dir}/tmp6.csv
+
+#	head -7 ${dir}/tmp6.csv > ${dir}/tmp7.csv
+#	sed -i 's/^/,,,,,,/' ${dir}/tmp7.csv
+#	join --header -t, /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250205.HHV3.for_joining.csv <( tail -n +8 ${dir}/tmp6.csv ) >> ${dir}/tmp7.csv
+#
+#	awk -F, '{print NF}' ${dir}/tmp7.csv | uniq
+#	#582
+#
+#	wc -l ${dir}/tmp7.csv
+#	#1655 tmp7.csv
+#
+#	wc -l /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250205.HHV3.for_joining.csv
+#	#1654 /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250205.HHV3.for_joining.csv
+#
+#	head -8 ${dir}/tmp7.csv > ${dir}/tmp8.csv
+#	tail -n +9 ${dir}/tmp7.csv | sort -t, -k2,2 -k3,3 -k4n,4 -k5n,5 -k6n,6 >> ${dir}/tmp8.csv
+#
+#	wc -l ${dir}/tmp8.csv
+#	#1655 tmp8.csv
+#
+#	awk -F, '{print NF}' ${dir}/tmp8.csv | uniq
+#	#582
+#
+#	head -12 ${dir}/tmp8.csv | cut -c1-110
+#	#,,,,,,sample,024JCM,024JCMdup,043MPL,043MPLdup,074KBP,074KBPdup,101VKC,101VKCdup,1301,1301dup,1302,1302dup,130
+#	#,,,,,,subject,024JCM,024JCM,043MPL,043MPL,074KBP,074KBP,101VKC,101VKC,1301,1301,1302,1302,1303,1303,1304,1304,
+#	#,,,,,,type,pemphigus serum,pemphigus serum,pemphigus serum,pemphigus serum,pemphigus serum,pemphigus serum,pem
+#	#,,,,,,study,PEMS,PEMS,PEMS,PEMS,PEMS,PEMS,PEMS,PEMS,MENS,MENS,MENS,MENS,MENS,MENS,MENS,MENS,MENS,MENS,MENS,MEN
+#	#,,,,,,group,Non Endemic Control,Non Endemic Control,Non Endemic Control,Non Endemic Control,Non Endemic Contro
+#	#,,,,,,age,62,62,61,61,44,44,46,46,33,33,67,67,61,61,60,60,54,54,72,72,50,50,65,65,51,51,39,39,46,46,39,39,50,5
+#	#,,,,,,sex,M,M,F,F,F,F,F,F,F,F,M,M,M,M,M,M,F,F,M,M,M,M,F,F,F,F,F,F,M,M,F,F,M,M,M,M,M,M,F,F,M,M,M,M,F,F,F,F,F,F,
+#	#id,Species,Protein names,Version (entry),Version (sequence),start,end,14,14,13,13,14,14,13,13,13,13,13,13,13,1
+#	#25844,Human herpesvirus 3,Alkaline nuclease (EC 3.1.-.-),47.0,1.0,1,56,2,0,1,0,11,7,0,27,3,2,1,3,0,0,0,3,0,4,0
+#	#25848,Human herpesvirus 3,Alkaline nuclease (EC 3.1.-.-),47.0,1.0,113,168,0,0,2,0,0,0,0,0,1,4,3,8,1,0,1,1,0,18
+#	#25849,Human herpesvirus 3,Alkaline nuclease (EC 3.1.-.-),47.0,1.0,141,196,0,28,93,8,0,4,0,0,13,2,43,1,25,5,19,
+#	#25850,Human herpesvirus 3,Alkaline nuclease (EC 3.1.-.-),47.0,1.0,169,224,0,0,3,14,2,2,3,6,5,8,5,21,3,2,0,14,0
+#
+#	mv tmp8.csv HHV3.csv
+
+#	head -7 ${dir}/tmp6.csv > ${dir}/tmp7.csv
+#	sed -i 's/^/,,,,,,/' ${dir}/tmp7.csv
+#	join --header -t, /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250207.for_joining.csv <( tail -n +8 ${dir}/tmp6.csv ) >> ${dir}/tmp7.csv
+	head -8 ${dir}/tmp6.csv > ${dir}/tmp7.csv
+	sed -i 's/^/,,,,,,/' ${dir}/tmp7.csv
+	join --header -t, /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250207.for_joining.csv <( tail -n +9 ${dir}/tmp6.csv ) >> ${dir}/tmp7.csv
+
+	awk -F, '{print NF}' ${dir}/tmp7.csv | uniq
+	#582
+
+	wc -l ${dir}/tmp7.csv
+	#126324 tmp7.csv
+
+	wc -l /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250207.for_joining.csv
+	#128258 /francislab/data1/refs/PhIP-Seq/VIR3_clean.20250207.for_joining.csv
+
+	#	lose almost 2000
+
+
+	head -9 ${dir}/tmp7.csv > ${dir}/Counts.csv
+	tail -n +10 ${dir}/tmp7.csv | sort -t, -k2,2 -k3,3 -k4n,4 -k5n,5 -k6n,6 >> ${dir}/Counts.csv
+	#head -8 ${dir}/tmp7.csv > ${dir}/Counts.csv
+	#tail -n +9 ${dir}/tmp7.csv | sort -t, -k2,2 -k3,3 -k4n,4 -k5n,5 -k6n,6 >> ${dir}/Counts.csv
+
+#fi
+
+
+phip_seq_normalize_counts.py -i ${dir}/Counts.csv
+
+
+#	Prep for usage in Multi_Plate_Case_Control_Peptide_Regression.R like Zscores.csv
+
+if [ -f ${dir}/Counts.normalized.subtracted.csv ] ; then
+
+#	is the
+#	y,x	#	probably not
+#	subject,type	#	yes
+#	needed?
+
+#hc out.plate1/Zscores.csv
+#y,x,id,1,10,100,1000,10000,10001,10002,10003,10004,10005,10006,10007,10008,10009,1001,10010,10011,10
+#subject,type,species,Papiine herpesvirus 2,Vaccinia virus,Human herpesvirus 3,Hepatitis B virus,Huma
+#14078-01,glioma serum,14078-01,-0.1735488911468417,-0.1735488911468417,-0.20851375718739676,-0.17354
+#14078-01,glioma serum,14078-01dup,-0.2909274899485718,-0.2909274899485718,-0.5265474573876068,-0.290
+#14118-01,glioma serum,14118-01,-0.20783649486361835,-0.20783649486361835,-0.18921235054240884,-0.207
+#14118-01,glioma serum,14118-01dup,-0.1909523835530943,-0.1909523835530943,0.4679949246045931,-0.1909
+#14127-01,glioma serum,14127-01,-0.2146477992014841,-0.2146477992014841,-0.4333066813813382,-0.214647
+#14127-01,glioma serum,14127-01dup,-0.11776590579451997,-0.11776590579451997,0.2664847297296404,-0.11
+#14142-01,glioma serum,14142-01,-0.13629977235983573,-0.13629977235983573,-0.27258615114003143,-0.136
+#14142-01,glioma serum,14142-01dup,,,-0.24344136937867783,,-0.24344136937867783,-0.24344136937867783,
+#
+#[gwendt@c4-dev3 /francislab/data1/working/20250128-Illumina-PhIP/20250128c-PhIP]$ hc out.plate1/Counts.normalized.subtracted.csv
+#id,species,protein,start,14078-01,14078-01dup,14118-01,14118-01dup,14127-01,14127-01dup,14142-01,141
+#,,,,14078-01,14078-01,14118-01,14118-01,14127-01,14127-01,14142-01,14142-01,14206-01,14206-01,14223-
+#,,,,/francislab/data1/working/20241204-Illumina-PhIP/20241204b-bowtie2/out/S83.VIR3_clean.1-84.bam,/
+#,,,,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,glioma serum,gliom
+#,,,,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,IPS,
+#,,,,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,case,c
+#,,,,60,60,61,61,63,63,78,78,49,49,58,58,72,72,58,58,61,61,62,62,71,71,53,53,54,54,75,75,61,61,53,53,
+#,,,,M,M,M,M,M,M,M,M,M,M,M,M,M,M,F,F,F,F,M,M,M,M,M,M,M,M,F,F,F,F,F,F,M,M,M,M,M,M,M,M,M,M,M,M,M,M,M,M,
+#,,,,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+#115701,Absidia glauca (Pin mould),Actin-2,1,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+
+	head -2 ${dir}/Counts.normalized.subtracted.csv | tail -1 > ${dir}/tmp1.csv
+	head -4 ${dir}/Counts.normalized.subtracted.csv | tail -1 >> ${dir}/tmp1.csv
+	head -1 ${dir}/Counts.normalized.subtracted.csv | tail -1 >> ${dir}/tmp1.csv
+	tail -n +10 ${dir}/Counts.normalized.subtracted.csv | sort -t, -k1,1 >> ${dir}/tmp1.csv
+
+	cat ${dir}/tmp1.csv | cut -d, -f1,2,5- | datamash transpose -t, > ${dir}/Counts.normalized.subtracted.trim.csv
+#	y,x	#	probably not
+#	subject,type	#	yes
+
+	sed -i -e '1s/^,/y,x/' -e '2s/^,/subject,type/' ${dir}/Counts.normalized.subtracted.trim.csv
+
+fi
+
+
+
+
+
+
 echo "Cleanup"
-\rm ${dir}/tmp?.csv
+\rm ${dir}/tmp*.csv
 
 echo "Done"
 
---
+++ scripts/phip_seq_normalize_counts.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+
+
+
+
+
+
+# include standard modules
+import argparse
+
+# initiate the parser
+parser = argparse.ArgumentParser(prog=os.path.basename(__file__))
+
+parser.add_argument('files', nargs='*', help='files help')
+parser.add_argument('-V','--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('-i', '--input', nargs=1, type=str, default=['Counts.csv'], help='input count csv filename to %(prog)s (default: %(default)s)')
+#parser.add_argument('-o', '--output', nargs=1, type=str, default=['output.csv'], help='output zscore csv filename to %(prog)s (default: %(default)s)')
+
+# read arguments from the command line
+args = parser.parse_args()
+
+
+
+
+
+
+
+print("Reading csv")
+#df = pd.read_csv( args.input[0],index_col='id')
+#df = pd.read_csv("ALL.csv",
+df = pd.read_csv(args.input[0],
+	header=[0,1,2,3,4,5,6,7,8],
+	index_col=[0,1,2,3,4,5,6])
+
+#df.columns = df.columns.set_names(["sample","subject","type","study","group","age","sex","plate"])
+df.columns = df.columns.set_names(["sample","subject","bampath","type","study","group","age","sex","plate"])
+df.index = df.index.set_names(["id","species","protein","entry version","sequence version","start","end"])
+
+df=df.droplevel([3,4,6])
+
+df.shape
+
+
+
+if( df.columns.get_level_values('sample').isin(['Blank50_1']).any() ):
+	df = df.drop('Blank50_1', level='sample', axis=1)
+
+
+
+
+
+df.shape
+
+# Create a function to replace the index values
+def replace_unnameds(value):
+	if value.startswith("Unnamed"):
+		return("_")
+	else:
+		return value
+
+df=df.rename(columns=replace_unnameds)
+
+#df=df.sort_index(axis=1,level=[2,3,4,1,0])
+df=df.sort_index(axis=1,level=[0,1,2,3,4])
+df=df.fillna(0)
+
+print(df.head())
+
+
+samples=df.columns.get_level_values('sample').unique().to_numpy()
+df_corr = pd.DataFrame(
+	columns=samples,
+	index=samples)
+
+for sample1 in samples:
+	for sample2 in samples:
+		corr, p1 = scipy.stats.pearsonr(
+			df.loc[:,df.columns.get_level_values('sample') == sample1],
+			df.loc[:,df.columns.get_level_values('sample') == sample2]
+		)
+		df_corr.at[sample1,sample2] = corr
+
+print(df_corr.head())
+df_corr.to_csv( os.path.splitext( args.input[0] )[0]+".pearson.csv", sep=',', index=True)
+
+
+
+
+print("Sums")
+sums=df.sum()
+print(sums.head())
+
+print("Normalized CPM")
+n=df/sums*1000000
+print(n.head())
+
+n=n.fillna(0)
+#	use the "reset_index" technique otherwise, the multi-index ends up on a line by itself.
+n.reset_index().to_csv( os.path.splitext( args.input[0] )[0]+".normalized.csv", sep=',', index=False)
+
+
+
+###	print("Plate 1 Inputs")
+###	print(n.loc[:,(n.columns.get_level_values('plate') == '1') & (n.columns.get_level_values('type') == 'input')])
+###	
+###	#print("Plate 1 Inputs Column Sums")
+###	#print(n.loc[:,(n.columns.get_level_values('plate') == '1') & (n.columns.get_level_values('type') == 'input')].sum())
+###	
+###	print("Plate 1 Inputs Row Medians")
+###	print(n.loc[:,(n.columns.get_level_values('plate') == '1') & (n.columns.get_level_values('type') == 'input')].median(axis=1))
+###	
+###	#print("Plate 1 Inputs Row Means")
+###	#print(n.loc[:,(n.columns.get_level_values('plate') == '1') & (n.columns.get_level_values('type') == 'input')].mean(axis=1))
+###	
+###	s=n.sub(n.loc[:,(n.columns.get_level_values('plate') == '1') & (n.columns.get_level_values('type') == 'input')].median(axis=1),axis=0)
+###	print(s.head())
+###	
+###	
+###	print("Sum of abs diffs")
+###	print(( df.loc[:,df.columns.get_level_values('sample') == 'CSE01_1'].droplevel('sample',axis=1) - df.loc[:,df.columns.get_level_values('sample') == 'CSE01_2'].droplevel('sample',axis=1)).abs().sum())
+###	print(( n.loc[:,n.columns.get_level_values('sample') == 'CSE01_1'].droplevel('sample',axis=1) - n.loc[:,n.columns.get_level_values('sample') == 'CSE01_2'].droplevel('sample',axis=1)).abs().sum())
+###	print(( s.loc[:,s.columns.get_level_values('sample') == 'CSE01_1'].droplevel('sample',axis=1) - s.loc[:,s.columns.get_level_values('sample') == 'CSE01_2'].droplevel('sample',axis=1)).abs().sum())
+###	
+
+
+
+
+
+
+print(n.columns.get_level_values('plate').unique())
+
+ss=[]
+for plate in n.columns.get_level_values('plate').unique():
+	a = n.loc[:,(n.columns.get_level_values('plate') == plate)]
+	ss.append(
+		a.sub(
+			a.loc[:,(a.columns.get_level_values('type') == 'input')].median(axis=1),
+				axis=0)
+	)
+
+s=pd.concat(ss,axis=1)
+s=s.sort_index(axis=1)
+s=s.fillna(0)
+
+print( s.head() )
+
+#s.to_csv('s5.csv', sep=',', index=True, header=True)
+#s.to_csv( os.path.splitext( args.input[0] )[0]+".normalized.subtracted.csv", sep=',', index=True, header=True)
+#	use the "reset_index" technique otherwise, the multi-index ends up on a line by itself.
+s.reset_index().to_csv( os.path.splitext( args.input[0] )[0]+".normalized.subtracted.csv", sep=',', index=False)
+
+samples=s.columns.get_level_values('sample').unique().to_numpy()
+s_corr = pd.DataFrame( columns=samples, index=samples )
+for sample1 in samples:
+	for sample2 in samples:
+		corr, p1 = scipy.stats.pearsonr(
+			s.loc[:,s.columns.get_level_values('sample') == sample1],
+			s.loc[:,s.columns.get_level_values('sample') == sample2]
+		)
+		s_corr.at[sample1,sample2] = corr
+	
+s_corr=s_corr.fillna(0)
+#print(s_corr)
+#s_corr.to_csv('s_corr5.csv', sep=',', index=True, header=True)
+s_corr.to_csv( os.path.splitext( args.input[0] )[0]+".normalized.subtracted.pearson.csv", sep=',', index=True)
+
+
+
+
+
+
+
+#	zf = pd.read_csv("zscores.csv",
+#		header=[0,1,2,3,4,5,6,7],
+#		index_col=[0])
+#	zf.columns = zf.columns.set_names(["sample","subject","type","study","group","age","sex","plate"])
+#	zf.index = zf.index.set_names(["id"])
+#	#zf=zf.sort_index(axis=1,level=[2,3,4,1,0])
+#	zf=zf.sort_index(axis=1,level=[0,1,2,3,4])
+#	zf=zf.fillna(0)
+#	
+#	samples=zf.columns.get_level_values('sample').unique().to_numpy()
+#	z_corr = pd.DataFrame( columns=samples, index=samples)
+#	
+#	print("Correlating each sample")
+#	for sample1 in samples:
+#		for sample2 in samples:
+#			corr, p1 = scipy.stats.pearsonr(
+#				zf.loc[:,zf.columns.get_level_values('sample') == sample1],
+#				zf.loc[:,zf.columns.get_level_values('sample') == sample2]
+#			)
+#			z_corr.at[sample1,sample2] = corr
+#	
+#	print("Writing")
+#	z_corr=z_corr.fillna(0)
+#	print(z_corr)
+#	z_corr.to_csv('z_corr4.csv', sep=',', index=True, header=True)
+#	
+#	
+#	
+#	
+#	
+#	
+#	#	#	#	#z = pd.read_csv("../out.plate1/All.count.Zscores.csv",
+#	#	#	#	z = pd.read_csv("../out.all/All.count.Zscores.csv",
+#	#	#	#		header=[0],
+#	#	#	#		index_col=[0])
+#	#	#	#	z.columns = z.columns.set_names(["sample"])
+#	#	#	#	z.index = z.index.set_names(["sample"])
+#	#	#	#	z.fillna(0,inplace=True)
+#	#	#	#	
+#	#	#	#	z_corr = pd.DataFrame(
+#	#	#	#		columns=z.columns.get_level_values('sample').to_numpy().unique(),
+#	#	#	#		index=z.columns.get_level_values('sample').to_numpy().unique())
+#	#	#	#	
+#	#	#	#	for sample1 in z.columns.get_level_values('sample').to_numpy().unique():
+#	#	#	#		for sample2 in z.columns.get_level_values('sample').to_numpy().unique():
+#	#	#	#			#corr = np.correlate(
+#	#	#	#			corr, p1 = scipy.stats.pearsonr(
+#	#	#	#				z.loc[:,z.columns.get_level_values('sample') == sample1],
+#	#	#	#				z.loc[:,z.columns.get_level_values('sample') == sample2]
+#	#	#	#			)
+#	#	#	#			z_corr.at[sample1,sample2] = corr
+#	#	#	#	
+#	#	#	#	print(z_corr)
+#	#	#	#	z_corr.to_csv('z_corr.csv', sep=',', index=True, header=True)
+#	#	
+#	#	
+#	#	
+#	#	#		print(n.columns.get_level_values('plate').unique())
+#	#	#		
+#	#	#		print("Reading Zscores files")
+#	#	#		zz=[]
+#	#	#		for plate in n.columns.get_level_values('plate').unique():
+#	#	#			#print("Reading ../out.plate"+plate+"/Zscores.t.csv")
+#	#	#			#a = pd.read_csv("../out.plate"+plate+"/Zscores.t.csv",
+#	#	#			#	header=[0,1,2],
+#	#	#			#	index_col=[0,1])
+#	#	#			print("Reading ../out.plate"+plate+"/All.count.Zscores.csv")
+#	#	#			a = pd.read_csv("../out.plate"+plate+"/All.count.Zscores.csv",
+#	#	#				header=[0],
+#	#	#				index_col=[0])
+#	#	#			print(a.head())
+#	#	#			#a.loc[('plate',''),:]=plate
+#	#	#			a.loc['plate',:]=plate
+#	#	#			a.columns = pd.MultiIndex.from_arrays([
+#	#	#				a.columns.get_level_values(0).to_numpy(),
+#	#	#				a.loc['plate'].to_numpy() ])
+#	#	#				#a.loc[('plate','')].to_numpy() ])
+#	#	#				#a.columns.get_level_values(1).to_numpy(),
+#	#	#				#a.columns.get_level_values(2).to_numpy(),
+#	#	#			#a.drop(('plate',''),inplace=True)
+#	#	#			a.drop('plate',inplace=True)
+#	#	#			#a.columns = a.columns.set_names(["subject","type","sample","plate"])
+#	#	#			#a.index = a.index.set_names(["id","species"])
+#	#	#			a.columns = a.columns.set_names(["sample","plate"])
+#	#	#			a.index = a.index.set_names(["id"])
+#	#	#			zz.append(a)
+#	#	#		
+#	#	#		print("Concatting")
+#	#	#		
+#	#	#		z=pd.concat(zz,axis=1)
+#	#	#		
+#	#	#		print(z.columns)
+#	#	#		print(z.columns.get_level_values('sample').unique().to_numpy())
+#	#	#		
+#	#	#		#z=z.sort_index(axis=1,level=[3,1,0,2])	#	sort columns plate, type, subject, sample
+#	#	#		#z=z.sort_index(axis=1,level=[1,0,2])	#	sort columns type, subject, sample
+#	#	#		z=z.sort_index(axis=1,level=[0])	#	sort columns sample
+#	#	#		z=z.fillna(0)
+#	#	#		
+#	#	#		print("Writing")
+#	#	#		print(z.head())
+#	#	#		z.to_csv('z.csv', sep=',', index=True, header=True)
+#	#	#		
+#	#	#		samples=z.columns.get_level_values('sample').unique().to_numpy()
+#	#	#		z_corr = pd.DataFrame(
+#	#	#			columns=samples,
+#	#	#			index=samples)
+#	#	#		
+#	#	#		print(z.columns.get_level_values('sample'))
+#	#	#		
+#	#	#		print("Correlating each sample")
+#	#	#		for sample1 in samples:
+#	#	#			for sample2 in samples:
+#	#	#				#print( sample1, " - ", sample2 )
+#	#	#				corr, p1 = scipy.stats.pearsonr(
+#	#	#					z.loc[:,z.columns.get_level_values('sample') == sample1],
+#	#	#					z.loc[:,z.columns.get_level_values('sample') == sample2]
+#	#	#				)
+#	#	#				z_corr.at[sample1,sample2] = corr
+#	#	#			
+#	#	#		print("Writing")
+#	#	#		print(z_corr)
+#	#	#		z_corr.to_csv('z_corr.csv', sep=',', index=True, header=True)
+
+
