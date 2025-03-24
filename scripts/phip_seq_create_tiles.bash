0f072fd2377207c88e30473cce5c45dcfb81099d
Jake <jakewendt@gmail.com>
2025-03-24 10:50:38 -0700

    Using named params

--- scripts/phip_seq_create_tiles.bash
+++ scripts/phip_seq_create_tiles.bash
@@ -1,38 +1,70 @@
 #!/usr/bin/env bash
 
+set -e  # exit if any command fails
+set -u  # Error on usage of unset variables
+set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+  echo "Loading required modules"
+#  module load CBI samtools r
+fi
+#set -x # print expanded command before executing it
+#set -v
 
 
-#	pepsyn is a bit outdated. Need biopython with Alphabet (1.69)
-#	but I can't get it to install anymore.
-#	I think that there is a different manner of creating these.
+function usage(){
+	set +x
+cat <<EOF
+Usage:
 
-#	https://pmc.ncbi.nlm.nih.gov/articles/PMC6568263
+$0 -t 56 -o 28 -i Human_alphaherpesvirus_3_proteins.faa
 
+	tilesize = integer
 
-#	pip uninstall biopython pepsyn
-#	python3 -m pip install --upgrade --user numpy scipy biopython click tqdm phip-stat
-#	cd ~/github/
-#	mkdir lasersonlab
-#	cd lasersonlab/
-#	git clone https://github.com/lasersonlab/pepsyn.git
-#	cd pepsyn
-#	python3 setup.py install --user
+	overlap = integer less than tilesize
+
+	input = an amino acid fasta
 
 
+	PhIP-Seq Characterization of Serum Antibodies Using Oligonucleotide Encoded Peptidomes
 
-#	python3 -m pip install --upgrade --user numpy scipy biopython==1.69 click tqdm pepsyn phip-stat
-#	
-#	http://weizhong-cluster.ucsd.edu/cd-hit/
-#	https://github.com/weizhongli/cdhit/releases
-#	wget https://github.com/weizhongli/cdhit/releases/download/V4.8.1/cd-hit-v4.8.1-2019-0228.tar.gz
+	https://pmc.ncbi.nlm.nih.gov/articles/PMC6568263
 
+	python3 -m pip uninstall -y biopython pepsyn
+	python3 -m pip install --upgrade --user numpy scipy biopython click tqdm phip-stat
+	git clone https://github.com/lasersonlab/pepsyn.git
+	cd pepsyn
+	python3 setup.py install --user
+	cd ..
+
+	http://weizhong-cluster.ucsd.edu/cd-hit/
+	https://github.com/weizhongli/cdhit/releases
+	wget https://github.com/weizhongli/cdhit/releases/download/V4.8.1/cd-hit-v4.8.1-2019-0228.tar.gz
+
+EOF
+
+	exit
+}
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+		-t|--tilesize)
+			shift; TILESIZE=$1; shift;;
+		-o|--overlap)
+			shift; OVERLAP=$1; shift;;
+		-i|--input)
+			shift; INPUT=$1; shift;;
+		*)
+			echo "Unknown param :${1}:"; usage ;;
+	esac
+done
 
 #	TILESIZE=56
 #	OVERLAP=28
 
-TILESIZE=$1
-OVERLAP=$2
-INPUT=$3			#	is this a fasta file? list of peptides? what? looks like its a fasta
+#	TILESIZE=$1
+#	OVERLAP=$2
+#	INPUT=$3			#	is this a fasta file? list of peptides? what? looks like its an amino acid fasta
+
 
 #	./phipseq.bash 56 28 Human_alphaherpesvirus_3_proteins.fa > phipseq.log
 
