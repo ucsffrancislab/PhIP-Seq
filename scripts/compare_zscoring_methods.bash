2dfe7630eec56a660316ea302538d0055a07e377
Jake <jakewendt@gmail.com>
2025-11-19 19:20:10 -0800

    Wrapped in time commands. Correct output file.

--- scripts/compare_zscoring_methods.bash
+++ scripts/compare_zscoring_methods.bash
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
+#SBATCH --export=NONE   # required when using 'module'
 
-#	/francislab/data1/working/20250409-Illumina-PhIP/20251113-PhIP/out.plate5
+hostname
+echo "Slurm job id:${SLURM_JOBID}:"
+date
+start_seconds=$SECONDS
 
-#	/francislab/data1/working/20250409-Illumina-PhIP/20251113-PhIP/out.plate6
+
+
+set -e	#	exit if any command fails
+set -u	#	Error on usage of unset variables
+set -o pipefail
+if [ -n "$( declare -F module )" ] ; then
+	echo "Loading required modules"
+	module load CBI r
+fi
+#set -x	#	print expanded command before executing it
+set -v
+
+
+#	/francislab/data1/working/20250409-Illumina-PhIP/20250411-PhIP/out.plate5
+
+#	/francislab/data1/working/20250409-Illumina-PhIP/20250411-PhIP/out.plate6
 
 OUTPUT=$1
 
@@ -14,6 +33,9 @@ echo "Computing Zscores"
 #  echo "Write-protected ${f} exists. Skipping."
 #else
 #  echo "Creating ${f}"
+
+
+#
 
   elledge_Zscore_analysis.R ${OUTPUT}/All.count.csv
   mv ${OUTPUT}/All.count.Zscores.csv ${OUTPUT}/tmp1.csv
@@ -28,7 +50,7 @@ echo "Computing Zscores"
   awk 'BEGIN{FS=OFS=","}{NF=NF-2;print $0}' ${OUTPUT}/tmp3.csv > ${OUTPUT}/tmp4.csv
   mv ${OUTPUT}/tmp3.csv ${OUTPUT}/All.count.Zscores.with_input_and_bin.jake.csv
 
-  head -1 ${OUTPUT}/tmp4.csv > ${OUTPUT}/All.count.Zscores.csv
+  head -1 ${OUTPUT}/tmp4.csv > ${OUTPUT}/All.count.Zscores.jake.csv
   tail -n +2 ${OUTPUT}/tmp4.csv | sort -t, -k1,1 >> ${OUTPUT}/All.count.Zscores.jake.csv
 
 
@@ -37,5 +59,15 @@ echo "Computing Zscores"
 #  \rm ${OUTPUT}/tmp2.csv
 #
 #fi
+
+
+
+echo "Done"
+date
+
+end_seconds=$SECONDS
+seconds=$((end_seconds-start_seconds))
+printf "%d seconds passed\n" $seconds
+echo "Elapsed: $(($seconds / 3600))hrs $((($seconds / 60) % 60))min $(($seconds % 60))sec"
 
 
